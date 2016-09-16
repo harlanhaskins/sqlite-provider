84ff972b3b4319fbb1d4440aa6231b13065655cf
Harlan <harlan@harlanhaskins.com>
2016-09-15 23:21:18 -0400

    Update dependency for Vapor 1.0

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporSQLite",
     dependencies: [
    		.Package(url: "https://github.com/vapor/sqlite-driver.git", majorVersion: 0, minor: 7),
-   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18),
+   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
     ]
 )
